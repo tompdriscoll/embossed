1880123131cb0862bfc990718646c53ffd15956d
Tom <tomdriscoll5@gmail.com>
2019-12-03 11:19:29 -0500

    added click listeners to color choose

---
+++ shapes.js
@@ -0,0 +1,19 @@
+document.getElementById('choose-white').addEventListener('click', function (){
+    console.log('hello')
+})
+
+document.getElementById('choose-black').addEventListener('click', function (){
+    console.log('hello')
+})
+
+document.getElementById('choose-salmon').addEventListener('click', function (){
+    console.log('hello')
+})
+
+document.getElementById('choose-cornflowerblue').addEventListener('click', function (){
+    console.log('hello')
+})
+
+document.getElementById('choose-aquamarine').addEventListener('click', function (){
+    console.log('hello')
+})
